ded4aeb490b27f1f1ad92dca9cb80da8a9dcf178
Yulia Starchenko <imay@amake.ru>
2024-10-22 15:19:28 +0300

    refactor: Container

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -2,9 +2,9 @@ import { ElementType, HTMLProps } from 'react';
 import cn from 'clsx';
 import s from './Container.module.scss';
 
-interface ContainerProps extends Omit<HTMLProps<HTMLElement>, 'as'> {
+type ContainerProps = Omit<HTMLProps<HTMLElement>, 'as'> & {
   as: ElementType;
-}
+};
 
 export const Container = ({ as: Elem, children, className, ...otherProps }: ContainerProps) => {
   return (
