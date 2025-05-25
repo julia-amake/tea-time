42e655cbf24f6dbb3ab6a29f3648137ea9949b99
Yulia Starchenko <imay@amake.ru>
2025-05-25 22:00:08 +0300

    build: migrate to next 15, turbo pack, react 19

--- src/shared/consts/appInfo.ts
+++ src/shared/consts/appInfo.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Tea Time';
-export const APP_SLOGAN = 'Больше, чем просто чай';
+export const APP_SLOGAN = '#заваримомент';
 export const APP_DESCRIPTION = 'Интернет-магазин чая с доставкой на дом';
